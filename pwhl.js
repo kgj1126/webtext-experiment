f14eefc9d7ffee9e77b1d025a80eb0901f4d46a4
kgj1126 <kgj11@pitt.edu>
2026-01-23 13:36:18 -0500

    Add files via upload

---
+++ pwhl.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#boston").click(function() {
+        $(this).attr("src", "fleet.jpg");
+    });    
+});
+$(document).ready(function() {
+    $("#minnesota").click(function() {
+        $(this).attr("src", "frost.jpg");
+    });    
+});
+$(document).ready(function() {
+    $("#new-york").click(function() {
+        $(this).attr("src", "sirens.jpg");
+    });    
+});
+$(document).ready(function() {
+    $("#ottawa").click(function() {
+        $(this).attr("src", "charge.jpg");
+    });    
+});
+$(document).ready(function() {
+    $("#montreal").click(function() {
+        $(this).attr("src", "victoire.jpg");
+    });    
+});
+$(document).ready(function() {
+    $("#toronto").click(function() {
+        $(this).attr("src", "sceptres.jpg");
+    });    
+});
